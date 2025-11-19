c70093b81dec9105594c6505d5e7df75eaf60c1a
taskpul <romtietz@gmail.com>
2025-11-19 09:35:26 +0100

    Hide item comments and harden related posts

--- template/single-product/tab/_feedback.php
+++ template/single-product/tab/_feedback.php
@@ -5,6 +5,17 @@
             <?php get_template_part( 'template/single-product/_feedback-static' ) ?>
             <?php endif; ?>
 
-            <?php comments_template( '/template/single-product/_review.php' ); ?>
+            <?php
+            /**
+             * The default WordPress comments template outputs the “Leave a comment”
+             * form along with other core comment markup. The /item/ templates no
+             * longer need that UI, so we completely short-circuit it unless a
+             * developer explicitly re-enables it via the
+             * `raphael_single_product_enable_wp_comments` filter.
+             */
+            if ( apply_filters( 'raphael_single_product_enable_wp_comments', false ) ) {
+                comments_template( '/template/single-product/_review.php' );
+            }
+            ?>
         </div>
 
